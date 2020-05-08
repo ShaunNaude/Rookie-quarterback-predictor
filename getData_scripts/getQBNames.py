3f4b8efd58f6eed44de0b3433acfc0f988144a03
Aeron John Land <aeronland@gmail.com>
2020-05-08 20:27:01 +0200

    finished pulling data on all QBs since 1999

--- getData_scripts/getQBNames.py
+++ getData_scripts/getQBNames.py
@@ -1,10 +1,11 @@
 from sportsreference.nfl.teams import Teams
 from datetime import datetime
 import pandas as pd
+import os
 
 start = datetime.now()
 
-for i in range(2000,2020):
+for i in range(2017,2020):
     year = str(i)
     teams = Teams(year)
     qbs = [[(player._name,player._player_id,player._position,) for player in team.roster.players] for team in teams._teams]
@@ -15,9 +16,18 @@ for i in range(2000,2020):
         mask = players_df['position'].apply(lambda x: 'qb' in x or 'QB' in x)
         qb_df = players_df[mask]
         allQbs = allQbs.append(qb_df)
-        allQbs.to_csv("qbs_" + year + ".csv")
+        allQbs.to_csv("../data/qbs_" + year + ".csv")
         
 
-end = datetime.now()
-totalTime = end-start
-print(totalTime)
+
+allqb_df = pd.DataFrame(columns = ['Name', 'player_id', 'position'])
+lis = []
+for i in range(1999,2020):
+    current = pd.read_csv('qbs_'+str(i)+'.csv')
+    allqb_df= allqb_df.append(current)
+
+allqb_df_unique = allqb_df.drop_duplicates(subset='player_id')
+
+allqb_names = allqb_df_unique.drop(columns=['Unnamed: 0','position'])
+
+allqb_names.to_csv('../data/all_qb_names.csv')
